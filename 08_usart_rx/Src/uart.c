28529f176839f9dc9c89d4569a28ad17582a2db5
Ankithkumarsingh <ankithsingh068@gmail.com>
2025-11-01 09:59:30 +0530

    initial commit

--- 08_usart_rx/Src/uart.c
+++ 08_usart_rx/Src/uart.c
@@ -1,11 +1,18 @@
-#include <uart.h>
-#define GPIOAEN			(1U<<0)
+#include "uart.h"
 
+
+
+#define GPIOAEN			(1U<<0)
 #define UART2EN			(1U<<17)
 
 #define CR1_TE			(1U<<3)
+#define CR1_RE			(1U<<2)
+
 #define CR1_UE			(1U<<13)
 #define SR_TXE			(1U<<7)
+#define SR_RXNE			(1U<<5)
+
+
 
 #define SYS_FREQ		16000000
 #define APB1_CLK		SYS_FREQ
@@ -15,16 +22,14 @@
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk,  uint32_t BaudRate);
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
 
-
 void uart2_write(int ch);
 
-int __io_putChar(int ch)
+
+int __io_putchar(int ch)
 {
-	uart_write(ch);
+	uart2_write(ch);
 	return ch;
 }
-
-
 
 void uart2_rxtx_init(void)
 {
@@ -32,15 +37,28 @@ void uart2_rxtx_init(void)
 	/*Enable clock access to gpioa */
 	RCC->AHB1ENR |= GPIOAEN;
 
+	/*Set PA2 mode to alternate function mode*/
+	GPIOA->MODER &=~(1U<<4);
+	GPIOA->MODER |= (1U<<5);
+
+	/*Set PA2 alternate function type to UART_TX (AF07)*/
+	GPIOA->AFR[0] |= (1U<<8);
+	GPIOA->AFR[0] |= (1U<<9);
+	GPIOA->AFR[0] |= (1U<<10);
+	GPIOA->AFR[0] &= ~(1U<<11);
+
+
 	/*Set PA3 mode to alternate function mode*/
 	GPIOA->MODER &=~(1U<<6);
 	GPIOA->MODER |= (1U<<7);
 
-	/*Set PA3 alternate function type to UART_TX (AF07)*/
+	/*Set PA3 alternate function type to UART_RX (AF07)*/
 	GPIOA->AFR[0] |= (1U<<12);
 	GPIOA->AFR[0] |= (1U<<13);
 	GPIOA->AFR[0] |= (1U<<14);
 	GPIOA->AFR[0] &= ~(1U<<15);
+
+
 
 
 	/****************Configure uart module ***************/
@@ -51,7 +69,40 @@ void uart2_rxtx_init(void)
 	uart_set_baudrate(USART2,APB1_CLK,UART_BAUDRATE);
 
 	/*Configure the transfer direction*/
-	 USART2->CR1 =  CR1_RE;
+	 USART2->CR1 =  (CR1_TE | CR1_RE);
+
+	/*Enable uart module*/
+	 USART2->CR1 |= CR1_UE;
+
+
+}
+
+void uart2_tx_init(void)
+{
+	/****************Configure uart gpio pin***************/
+	/*Enable clock access to gpioa */
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*Set PA2 mode to alternate function mode*/
+	GPIOA->MODER &=~(1U<<4);
+	GPIOA->MODER |= (1U<<5);
+
+	/*Set PA2 alternate function type to UART_TX (AF07)*/
+	GPIOA->AFR[0] |= (1U<<8);
+	GPIOA->AFR[0] |= (1U<<9);
+	GPIOA->AFR[0] |= (1U<<10);
+	GPIOA->AFR[0] &= ~(1U<<11);
+
+
+	/****************Configure uart module ***************/
+	/*Enable clock access to uart2 */
+	RCC->APB1ENR |= UART2EN;
+
+	/*Configure baudrate*/
+	uart_set_baudrate(USART2,APB1_CLK,UART_BAUDRATE);
+
+	/*Configure the transfer direction*/
+	 USART2->CR1 =  CR1_TE;
 
 	/*Enable uart module*/
 	 USART2->CR1 |= CR1_UE;
@@ -60,7 +111,15 @@ void uart2_rxtx_init(void)
 }
 
 
+char uart2_read(void)
+{
+	  /*Make sure the receive data register is not empty*/
+	while(!(USART2->SR & SR_RXNE)){}
 
+	/*Read data*/
+	return USART2->DR;
+
+}
 
 void uart2_write(int ch)
 {
